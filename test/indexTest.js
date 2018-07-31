3d5eb1ac71e48e76e5b85998c4f4aa5b5c614503
unknown <sanpadhy@WKWIN8244532.global.publicisgroupe.net>
2018-07-31 09:26:04 +0530

    added ES6 support for mocha and chai

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const index = require('../index');
 
 describe('Index', function(){
-    it('it should return hello1', function(){
+    it('it should return hello1', ()=>{
         assert.equal(index(), 'hello1');
     })
 })
